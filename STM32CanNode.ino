ffe957422756ed75c62b4af3116586d9d9d5ad7a
markusmj <markus@markusjahn.net>
2017-12-22 12:04:32 +0200

    Initial commit

---
+++ STM32CanNode.ino
@@ -0,0 +1,63 @@
+// demo: CAN-BUS Shield, send data
+#include <mcp_can.h>
+#include <SPI.h>
+
+
+const int SPI_CS_PIN = PA8;
+
+byte runumber = 0;
+byte lowdata = 0;
+byte highdata = 0;
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+void setup()
+{
+    pinMode(PA0, INPUT_ANALOG);
+    pinMode(PA1, INPUT_ANALOG);
+    pinMode(PA2, INPUT_ANALOG);
+    pinMode(PA3, INPUT_ANALOG);
+    pinMode(PA4, INPUT_ANALOG);
+    pinMode(PA5, INPUT_ANALOG);
+    pinMode(PA6, INPUT_ANALOG);
+    pinMode(PA7, INPUT_ANALOG);
+    pinMode(PB0, INPUT_ANALOG);
+    pinMode(PB1, INPUT_ANALOG);
+    
+    SPI.setModule(2);
+    SPI.setClockDivider(SPI_CLOCK_DIV2);
+    Serial.begin(115200);
+    
+
+    while (CAN_OK != CAN.begin(CAN_1000KBPS))              // init can bus : baudrate = 1000k
+    {
+        
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    
+    Serial.println("CAN BUS Shield init ok!");
+    
+}
+
+unsigned char stmp[8] = {6, 2, 6, 8, 6, 7, 6, 6};
+void loop()
+{   
+    int Timing = micros();
+    
+    //unsigned char stmp[8] = {lowByte(analogRead(PA0)), highByte(analogRead(PA0)), lowByte(analogRead(PA1)), highByte(analogRead(PA1)), lowByte(analogRead(PA2)), highByte(analogRead(PA2)), lowByte(analogRead(PA3)), highByte(analogRead(PA3))};
+    unsigned char stmp2[8] = {lowByte(analogRead(PA4)), highByte(analogRead(PA4)), lowByte(analogRead(PA5)), highByte(analogRead(PA5)), lowByte(analogRead(PA6)), highByte(analogRead(PA6)), lowByte(analogRead(PA7)), highByte(analogRead(PA7))};
+    unsigned char stmp3[8] = {lowByte(analogRead(PB0)), highByte(analogRead(PB0)), lowByte(analogRead(PB1)), highByte(analogRead(PB1)), 6, 6, 6, 6};
+
+    // runumber++;
+    // send data:  id = 0x00, standard frame, data len = 8, stmp: data buf
+    
+    CAN.sendMsgBuf(0x29A,0, 8, stmp);
+    CAN.sendMsgBuf(0x666,0, 8, stmp);
+    CAN.sendMsgBuf(0x66,0, 8, stmp);
+    Timing = micros() - Timing;
+    Serial.println(Timing);
+    delay(100);                       // send data once per second
+}
+
