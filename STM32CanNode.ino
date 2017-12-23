fb46728935398ce881df60c4338afed24586fdec
markusmj <markus@markusjahn.net>
2017-12-23 03:38:12 +0200

    Faster ADC Read

--- STM32CanNode.ino
+++ STM32CanNode.ino
@@ -1,26 +1,27 @@
 // demo: CAN-BUS Shield, send data
 #include <mcp_can.h>
 #include <SPI.h>
+#include <STM32ADC.h>
 
+uint8 pins[] = {PA0,PA1,PA2,PA3,PA4,PA5,PA6,PA7,PB0,PB1}; //Analog input pins
+unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+unsigned char stmp2[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+unsigned char stmp3[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
+const int maxSamples = 10; // 10 Analog inputs
 const int SPI_CS_PIN = PA8;
 
+// Array for the ADC data
+uint16 dataPoints[maxSamples]; 
 
 MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
 
+volatile static bool dma1_ch1_Active; //flag for interrupt
+STM32ADC NodeADC(ADC1);
+
 void setup()
 {
-    pinMode(PA0, INPUT_ANALOG);
-    pinMode(PA1, INPUT_ANALOG);
-    pinMode(PA2, INPUT_ANALOG);
-    pinMode(PA3, INPUT_ANALOG);
-    pinMode(PA4, INPUT_ANALOG);
-    pinMode(PA5, INPUT_ANALOG);
-    pinMode(PA6, INPUT_ANALOG);
-    pinMode(PA7, INPUT_ANALOG);
-    pinMode(PB0, INPUT_ANALOG);
-    pinMode(PB1, INPUT_ANALOG);
-    
+   
         
     SPI.setModule(2);   //Define SPI to SPI2
     SPI.setClockDivider(SPI_CLOCK_DIV2);   //Define Speed of SPI
@@ -36,17 +37,28 @@ void setup()
     }
     
     Serial.println("CAN BUS Shield init ok!");
+
+    //calibrate ADC
+    NodeADC.calibrate(); 
+    NodeADC.setSampleRate(ADC_SMPR_1_5); //sample ratio
+    NodeADC.setPins(pins, maxSamples);   //pins to be converted
+    NodeADC.setScanMode();               //Set the ADC in Scan Mode
     
 }
 
 
 void loop()
 {   
-    //int Timing = micros();
+    int Timing = micros();
+    dma1_ch1_Active = 1;
+    NodeADC.setDMA(dataPoints, 10, (DMA_MINC_MODE | DMA_TRNS_CMPLT), DMA1_CH1_Event); 
+    NodeADC.startConversion();
+    while (dma1_ch1_Active == 1); //wait for DMA to complete.  
+
     
-    unsigned char stmp[8] = {lowByte(analogRead(PA0)), highByte(analogRead(PA0)), lowByte(analogRead(PA1)), highByte(analogRead(PA1)), lowByte(analogRead(PA2)), highByte(analogRead(PA2)), lowByte(analogRead(PA3)), highByte(analogRead(PA3))};
-    unsigned char stmp2[8] = {lowByte(analogRead(PA4)), highByte(analogRead(PA4)), lowByte(analogRead(PA5)), highByte(analogRead(PA5)), lowByte(analogRead(PA6)), highByte(analogRead(PA6)), lowByte(analogRead(PA7)), highByte(analogRead(PA7))};
-    unsigned char stmp3[8] = {lowByte(analogRead(PB0)), highByte(analogRead(PB0)), lowByte(analogRead(PB1)), highByte(analogRead(PB1)), 0, 0, 0, 0};
+    unsigned char stmp[8] = {lowByte(dataPoints[0]), highByte(dataPoints[0]), lowByte(dataPoints[1]), highByte(dataPoints[1]), lowByte(dataPoints[2]), highByte(dataPoints[2]), lowByte(dataPoints[3]), highByte(dataPoints[3])};
+    unsigned char stmp2[8] = {lowByte(dataPoints[4]), highByte(dataPoints[4]), lowByte(dataPoints[5]), highByte(dataPoints[5]), lowByte(dataPoints[6]), highByte(dataPoints[6]), lowByte(dataPoints[7]), highByte(dataPoints[7])};
+    unsigned char stmp3[8] = {lowByte(dataPoints[8]), highByte(dataPoints[8]), lowByte(dataPoints[9]), highByte(dataPoints[9]), 0, 0, 0, 0};
 
     // Send data:  id = 0x00, standard frame, data len = 8, stmp: data buf
     
@@ -54,9 +66,18 @@ void loop()
     CAN.sendMsgBuf(0x666,0, 8, stmp2);
     CAN.sendMsgBuf(0x66,0, 8, stmp3);
     
-    //Timing = micros() - Timing;       //Timing checks how long it takes to make analog reads and send data to canbus       
-    //Serial.println(Timing);           //Uncomment if needed. Also from loops first line.
+    Timing = micros() - Timing;       //Timing checks how long it takes to make analog reads and send data to canbus       
+    Serial.println(Timing);           //Uncomment if needed. Also from loops first line.
     
-    delay(100);                        // send data once per 0.1 second
+    for(unsigned int i = 0; i < maxSamples; i ++) {
+            Serial.print("sample[");
+            Serial.print(i);
+            Serial.print("] = ");
+            Serial.println(dataPoints[i]);
+    }
+    delay(1000);                        // send data once per 0.1 second
 }
 
+static void DMA1_CH1_Event() {
+  dma1_ch1_Active = 0;
+}
